a471da46abaa7772e8c4c8f9ceb01f1e0bb0a5f4
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-02-25 10:08:19 +0700

    Jobsheet3_Praktikum2.1_modifikasi tabel

--- database/migrations/2025_02_25_023642_create_m_user_table.php
+++ database/migrations/2025_02_25_023642_create_m_user_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('m_user', function (Blueprint $table) {
-            //
+            $table->id('user_id');
+            $table->unsignedBigInteger('level_id')->index(); 
+            $table->string('username', 20)->unique();
+            $table->string('nama', 100);
+            $table->string('password');
+            $table->timestamps();
+
+            $table->foreign('level_id')->rferences('level_id')->on('m_level');
         });
     }
 
