7d4770f9e6b978ca6d7363f7288ecf906093e2ef
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-02-25 17:53:39 +0700

    Jobsheet3_Praktikum6_Langkah10_Modifikasi file UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,13 +11,10 @@ class UserController extends Controller
     public function index()
     {
         $data = [
-            'username' => 'customer-1',
-            'nama' => 'Pelanggan',
-            'password' => Hash::make('12345'),
-            'level_id'=> 4
+            'nama'=> 'Pelanggan Pertama',
         ];
 
-        UserModel::insert($data);
+        UserModel::where('username','customer-1')->update($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
