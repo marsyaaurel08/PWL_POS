2a5861f65e7f380656b7e68a59de8ff466ed6929
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-03-04 08:53:18 +0700

    Jobsheet4_Praktikum2.6_Langkah6_Menambahkan script pada UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,4 +22,8 @@ class UserController extends Controller
         $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
 }
