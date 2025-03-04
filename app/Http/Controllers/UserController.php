43e781689065338e44ec4d5373e391b5725a37c2
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-03-04 15:08:31 +0700

    Jobsheet4_Praktikum2.6_Langkah19_Menambah script pada UserController untuk delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -51,5 +51,13 @@ class UserController extends Controller
 
         return redirect('/user');
     }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+    }
 }
 
