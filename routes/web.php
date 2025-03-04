a784bdba5b2b09ec8e269dd01a9e8939fdf5f39f
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-03-04 08:49:20 +0700

    Jobsheet4_Praktikum2.6_Langkah5_Menambahkan script pada routes

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,5 @@ Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 
 Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
