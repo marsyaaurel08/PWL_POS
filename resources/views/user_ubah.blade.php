3119659d4986e04f41a30107324bceed05967f57
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-03-04 10:32:16 +0700

    Jobsheet4_Praktikum2.6_Langkah11

--- resources/views/user_ubah.blade.php
+++ resources/views/user_ubah.blade.php
@@ -4,28 +4,37 @@
         <title>Form Ubah Data User</title>
     </head>
     <body>
-        <h1>Form Ubah Data User</h1>
-        <a href="/user">Kembali</a>
-        <br><br>
+    <h1>Form Ubah Data User</h1>
+    <a href="/user">Kembali</a>
+    <br><br>
 
-        <form method="post" action="ubah_simpan/ {{ $data->uer_id }}">
+    <form method="post" action="/user/ubah_simpan/{{ $data->user_id }}">
 
         {{ csrf_field() }}
-        {{ method_field('PUT') }}
+        {{ method_field("PUT") }}
 
         <label>Username</label>
-        <input type="text" name="username" placeholder="Masukkan Username">
         <br>
+        <input type="text" name="username" placeholder="Masukkan Username" value="{{ $data->username }}">
+        <br>
+
         <label>Nama</label>
-        <input type="text" name="nama" placeholder="Masukkan Nama">
         <br>
+        <input type="text" name="nama" placeholder="Masukkan Nama" value="{{ $data->nama }}">
+        <br>
+
         <label>Password</label>
-        <input type="password" name="password" placeholder="Masukkan Password">
         <br>
+        <input type="password" name="password" placeholder="Masukkan Password" value="{{ $data->password }}">
+        <br>
+
         <label>Level ID</label>
-        <input type="number" name="level_id" placeholder="Masukkan ID Level">
+        <br>
+        <input type="number" name="level_id" placeholder="Masukkan ID Level" value="{{ $data->level_id }}">
         <br><br>
-        <input type="submit" class="btn btn-success" value="Simpan">
-        </form>
-    </body>
+
+        <input type="submit" class="btn btn-success" value="Ubah">
+    </form>
+</body>
+
 </html>
