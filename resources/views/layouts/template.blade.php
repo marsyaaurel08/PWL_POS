29647a425e9ffdecdce84060423664efe076cd75
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-03-08 16:56:13 +0700

    Jobsheet5_Praktikum1_Langkah11_Modifikasi file template.blade.php pada baris 25-26

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -22,9 +22,9 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
-      <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    <a href="{{ url('/') }}" class="brand-link">
+      <img src="{{ assets('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
 
     <!-- Sidebar -->
