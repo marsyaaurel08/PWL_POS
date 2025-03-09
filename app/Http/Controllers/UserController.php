9e0beda2182b81ddf9114eb805ec9cf972b3eabb
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-03-09 16:32:41 +0700

    Jobsheet5_Praktikum3_Langkah4_Modifikasi UserController bagian fungsi index()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,18 +1,30 @@
 <?php
 
-
 namespace App\Http\Controllers;
 
+use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Yajra\DataTables\Facades\DataTables;
 
 class UserController extends Controller
 {
+    // Menampilkan halaman awal user
     public function index()
     {
-        $user = UserModel::with('level')->get();
-        return view('user', ['data' => $user]);
+        $breadcrumb = (object) [
+            'title' => 'Daftar User',
+            'list' => ['Home', 'User']
+        ];
+
+        $page = (object) [
+            'title' => 'Daftar user yang terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'user';
+
+        return view('user.index',['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu]);
         
     }
 
