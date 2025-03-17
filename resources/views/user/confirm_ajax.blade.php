8a460197183ebaada04f24a1b46b4e6cf1c550b1
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-03-17 22:00:33 +0700

    Jobsheet6_Praktikum3_Perbaikan Fix

--- resources/views/user/confirm_ajax.blade.php
+++ resources/views/user/confirm_ajax.blade.php
@@ -36,7 +36,7 @@
                 <table class="table table-sm table-bordered table-striped">
                     <tr><th class="text-right col-3">Level Pengguna :</th><td class="col-9">{{ $user->level->level_nama }}</td></tr>
                     <tr><th class="text-right col-3">Username :</th><td class="col-9">{{ $user->username }}</td></tr>
-                    <tr><th class="text-right col-3">Nama :</th><td class="col-9">{{ $user>nama }}</td></tr>
+                    <tr><th class="text-right col-3">Nama :</th><td class="col-9">{{ $user->nama }}</td></tr>
                 </table>
             </div>
             <div class="modal-footer">
