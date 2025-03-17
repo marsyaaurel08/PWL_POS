0c69f4492de603d9daf936955f002370cd2fb897
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-03-17 21:36:57 +0700

    Jobsheet6_Praktikum3_Langkah5_Membuat fungsi delete_ajax() pada UserController.php untuk mengakomodir request hapus data user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -199,6 +199,28 @@ class UserController extends Controller
         return view('user.confirm_ajax', ['user' => $user]);
     }
 
+    // Delete ajax
+    public function delete_ajax(Request $request, $id)
+    {
+        //cek apakah request dari ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $user = UserModel::find($id);
+            if ($user) {
+                $user->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
+    }
+
     // Menampilkan detail user
     public function show(string $id)
     {
