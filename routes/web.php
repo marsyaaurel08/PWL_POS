377b424da306fa0fd8f4af07dcabc3ecc03399c3
Marsya Aurelia Sefira <sefiramarsya@gmail.com>
2025-04-10 16:27:29 +0700

    Jobsheet8_Praktikum1_Langkah5_modifikasi route untuk mengakomodir proses upload file pada menu barang

--- routes/web.php
+++ routes/web.php
@@ -106,7 +106,9 @@ Route::middleware(['authorize:ADM,MNG,STF,SLS,SPV,SMD'])->group(function() {
         Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']); 
         Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']); 
         Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']); 
-        Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']); 
+        Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
+        Route::get('/import', [BarangController::class, 'import']); //ajax form upload excel
+        Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
     });
 
 }); 
